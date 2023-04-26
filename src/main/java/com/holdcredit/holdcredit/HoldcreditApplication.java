bcfd9591f9bb715c496eecb63b0d6e75ef394ec1
추경현 <pleasantckh@naver.com>
2023-04-26 17:25:52 +0900

    Project 생성 및 ReadMe 작성

---
+++ src/main/java/com/holdcredit/holdcredit/HoldcreditApplication.java
@@ -0,0 +1,13 @@
+package com.holdcredit.holdcredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldcreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoldcreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/holdcredit/holdcredit/HoldcreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holdcredit.holdcredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldcreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
