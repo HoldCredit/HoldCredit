463ea0645f4830d065e78f7cee29a72001c3d22a
BEENY\beeny <wmks2719@gmail.com>
2023-05-08 18:19:07 +0900

    헤더, 푸터, css수정 및 로그인 회원가입 헤더 연동

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -6,18 +6,22 @@ import Home from './screens/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CreditRating from './screens/CreditRating';
+import LoginPage from './login/LoginPage';
+import JoinForm from './login/JoinForm';
 
 function App(){
   return(
     <Router>
       <Header />
-      <div className="main">
+      
       <Routes>
         <Route path ="/" element = {<Home />} />
         <Route path ="/Service" element = {<Service />} />
         <Route path ="/CreditRating" element = {<CreditRating />} />
+        <Route path ="/LoginPage" element = {<LoginPage />} />
+        <Route path ="/JoinForm" element = {<JoinForm />} />
         </Routes>
-      </div>
+ 
       <Footer />
     </Router>
     
