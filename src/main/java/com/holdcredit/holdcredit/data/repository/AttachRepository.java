742c5475bf7d99f5e040e0467ff676c67ea5ded9
HI-RAN\82104 <rlagofks4623@gmail.com>
2023-05-11 21:14:39 +0900

    보드 entity 관계설정 완료

---
+++ src/main/java/com/holdcredit/holdcredit/data/repository/AttachRepository.java
@@ -0,0 +1,7 @@
+package com.holdcredit.holdcredit.data.repository;
+
+import com.holdcredit.holdcredit.data.entity.AttachEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttachRepository extends JpaRepository<AttachEntity, Long> {
+}
