c18515b827033f7f46f9b132102dba4e1037c75a
HI-RAN\82104 <rlagofks4623@gmail.com>
2023-05-16 19:50:06 +0900

    Customer 테이블에 수정일 추가, enum 영어로 변경

--- src/main/java/com/holdcredit/holdcredit/domain/entity/enumeration/JobDomain.java
+++ src/main/java/com/holdcredit/holdcredit/domain/entity/enumeration/JobDomain.java
@@ -1,5 +1,7 @@
 package com.holdcredit.holdcredit.domain.entity.enumeration;
 
 public enum JobDomain {
-    개인사업자, 법인사업자, 공무원, 회사원, 기타;
+    entrepreneur,publicOfficial,worker,etc;
+
+    /* 사업가, 공무원, 회사원, 기타 */
 }
