e4a9e4859412ba7862344a8d78a849b649779633
추경현 <pleasantckh@naver.com>
2023-05-31 09:44:07 +0900

    .

--- src/main/frontend/src/login/LoginPage.js
+++ src/main/frontend/src/login/LoginPage.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import '../styles/Login.css';
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 function LoginPage() {
 
-    function handleJoinClick() {
-        window.location.href = '/JoinForm';
+    const navigate = useNavigate();
+
+    // useEffect(() => {
+    //     return () => {
+    //         axios.post('http://localhost:8080/auth/login')
+    //
+    //     };
+    // }, []);
+
+    const [loginId, setId] = useState('')
+    const [password, setPassword] = useState('')
+
+    const idHandler = (event) => {
+        setId(event.target.value);
+    }
+    const passwordHandler = (event) => {
+        setPassword(event.target.value);
     }
 
-    function handleFindAccountClick() {
-        window.location.href = '/FindId';
-    }
+    console.log(loginId);
+    console.log(password);
 
-    function handleFindPasswordClick() {
-        window.location.href = '/FindPwd';
-    }
 
     return (
         <div className="container-doc">
@@ -32,18 +45,18 @@ function LoginPage() {
                                 <form>
                                     <div className="box_Id">
                                     <label className="screen_out">계정정보 입력</label>
-                                        <input type="text" id="loginId" name="loginId" placeholder="아이디, 이메일" className="tf_g" />
+                                        <input type="text" onChange={idHandler} value={loginId} placeholder="ID" className="tf_g" />
                                     </div>
                                     
                                     <div className="box_Pwd">
                                     <label className="screen_out">비밀번호 입력</label>
-                                        <input type="password" id="password" name="password" placeholder="비밀번호" className="tf_g"/>
+                                        <input type="password" onChange={passwordHandler} value={password} placeholder="password" className="tf_g"/>
                                     </div>
                                     <div className="login_checkbox">
                                         <div id="login_ck">
                                             <input type="checkbox" id="checkbox" className="check_sign"/>
                                             <label className="loginCk">
-                                                <span className="loginchecked">로그인 상태 유지</span>
+                                                <span className="loginchecked" >로그인 상태 유지</span>
                                             </label>
                                         </div>
                                     </div>
@@ -54,13 +67,13 @@ function LoginPage() {
                                     </div>
                                 </form>
                                 <div className="info_user">
-                                    <button className="link_join" onClick={handleJoinClick}>회원가입</button>
+                                    <button className="link_join" onClick={() => navigate('/JoinForm')}>회원가입</button>
                                     <ul className="list_user">
                                         <li>
-                                            <button className="link_user" onClick={handleFindAccountClick}>계정 찾기</button>
+                                            <button className="link_user" onClick={() => navigate('/FindId')}>계정 찾기</button>
                                         </li>
                                         <li>
-                                            <button className="link_user" onClick={handleFindPasswordClick}>비밀번호 찾기</button>
+                                            <button className="link_user" onClick={() => navigate('/FindPwd')}>비밀번호 찾기</button>
                                         </li>
                                     </ul>
                                 </div>
