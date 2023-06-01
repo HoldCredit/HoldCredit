f4706f2c9ec54e33baca09ed2e6b2d1b56fd7d91
추경현 <pleasantckh@naver.com>
2023-06-02 04:25:43 +0900

    게시판 수정에 따른 대시보드 수정

--- src/main/frontend/src/dashboard/components/BoardWrite.js
+++ src/main/frontend/src/dashboard/components/BoardWrite.js
@@ -9,19 +9,20 @@ import Table from "@mui/material/Table";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import BoardService from "../../service/BoardService";
-
-const ariaLabel = {'aria-label': 'description'};
+import '../../screens/css/Board.css';
 
 export default function BoardWrite() {
 
   const menu = useSelector((state) => state.selectMenu);
   const navigate = useNavigate();
 
+  const writer = useSelector((state) => state.customerName);
 
   const [data, setData] = useState([]);
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [pwd, setPwd] = useState('');
+  const [files, setFiles] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -48,21 +49,42 @@ export default function BoardWrite() {
   const changePwdHandler = (event) => {
     setPwd(event.target.value);
   }
+  const changeFileHandler = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const fileList = Array.from(event.target.files);
+      setFiles((prevFiles) => [...prevFiles, ...fileList]);
+    }
+  };
 
   const postBoard = (event) => {
     event.preventDefault();
-    let data = {
-      title: title,
-      content: content,
-      pwd: pwd,
-    };
+
     switch (menu.menuName) {
       case 'Notice':
-        return BoardService.createNotice(data).then(res => {
+        let formData = new FormData();
+        formData.append('title', title);
+        formData.append('content', content);
+        formData.append('pwd', pwd);
+        files.forEach((file) => {
+          formData.append('file', file);
+        });
+        const config = {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        };
+        return BoardService.createNotice(formData, config)
+          .then(res => {
           alert('등록되었습니다.');
           navigate(-1);
         });
+
       case 'Qna' :
+        let data = {
+          title: title,
+          content: content,
+          pwd: pwd,
+        };
         return BoardService.createQna(data).then(res => {
           alert('등록되었습니다.');
           navigate(-1);
@@ -71,35 +93,61 @@ export default function BoardWrite() {
   };
 
 
+
+
   return (
     <>
       <Title>{menu.name}</Title>
       <Grid item sx={{display: "flex", margin: "10px", paddingTop: "50px"}}>
         <Typography variant="h5" sx={{paddingLeft: '30px'}}>제목</Typography>
         <Typography variant="h5" sx={{paddingLeft: '25px'}}>
-          <Input defaultValue="제목을 입력하세요." sx={{width: '300px'}} type="text" name="title"
+          <Input sx={{width: '300px'}} type="text" name="title"
                  value={title} onChange={changeTitleHandler}/>
         </Typography>
       </Grid>
       <Table size="large">
         <TableRow>
-          <TableCell>작성자</TableCell>
+          <TableCell value={writer}>작성자</TableCell>
           <TableCell sx={{paddingLeft: '0', paddingRight: '200px'}}>
-            <Input defaultValue={data.writer} type="text"/>
+            <Input readOnly defaultValue={writer} type="text"/>
           </TableCell>
+
           <TableCell>비밀번호</TableCell>
           <TableCell sx={{paddingLeft: '0', paddingRight: '200px'}}>
             <Input defaultValue="1234" type="password" name="pwd"
                    value={pwd} onChange={changePwdHandler}/>
           </TableCell>
+          <TableCell>
+          <label htmlFor="fileInput" className="fileInputButton">
+            파일첨부
+          </label>
+          <input id="fileInput" type="file" name="file"
+            multiple style={{ display: 'none' }} onChange={changeFileHandler} />
+            </TableCell>
+
         </TableRow>
       </Table>
       <Box className="write_textfield">
-        <Typography variant="body1" sx={{margin: '15px'}}>
+
+        <Typography variant="body1" sx={{margin: '30px'}}>
         </Typography>
         <TextField multiline fullWidth name="content" value={content}
                    variant="outlined" onChange={changeContentHandler} />
-
+        <div>
+          {files.length > 0 &&
+            files.map((file, index) => (
+              <div key={index} className="se-module" style={{marginTop:"0px"}}>
+                <span className="se-file-icon">
+                  <strong className="se-blind"></strong>
+                </span>
+                <div className="file">
+                  <div className="selectedFile">
+                    <span>{file.name}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
       </Box>
 
       <Stack spacing={2} direction="row" justifyContent="center" alignItem="" marginTop="auto">
