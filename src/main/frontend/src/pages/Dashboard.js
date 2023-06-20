edebe4ab3eb1924ed6b4b773f3b3fb573d2e1c7a
smwkdiek <smwkdiek@naver.com>
2023-06-20 17:17:45 +0900

    알림창 수정 css추가

--- src/main/frontend/src/pages/Dashboard.js
+++ src/main/frontend/src/pages/Dashboard.js
@@ -17,9 +17,9 @@ function Dashboard() {
           <DashBoardHeader/>
 
           {/* Content */}
-          <Box component="main" className="under"
+          <Box component="main"
                sx={{padding:"25px 0px",position:"relative", zIndex: "0",
-                 flexGrow: 1, height: '100vh', overflow: 'auto', }}>
+                 flexGrow: 1, height: '100vh', overflow: 'auto', backgroundColor:'#f8f9fa'}}>
             {/*<Provider PageTitleStore={PageTitleStore}>*/}
               <Outlet/>
             {/*</Provider>*/}
