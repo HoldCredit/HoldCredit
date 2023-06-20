6083b4a65cbdbf06a78c26fe376d3815186100a9
Geunju Oh <gjoh240@gmail.com>
2023-06-20 21:41:58 +0900

    내자산, 한도 페이지 카드 형식으로 수정

--- src/main/frontend/src/dashboard/maincontents/MyCredit.js
+++ src/main/frontend/src/dashboard/maincontents/MyCredit.js
@@ -6,6 +6,8 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from 'react';
 import { Container } from "@mui/material";
+import styles from "../../styles/MyAssets.css"; // Import CSS module
+import { Grid, Card, CardContent, Typography } from "@mui/material";
 
 
 export default function MyCredit() {
@@ -29,40 +31,70 @@ export default function MyCredit() {
     name: customerName,
   };
 
+  //AnonymousData 정보 담기
+    const [creditInfo, setCreditInfo] = React.useState(null);
+
+      useEffect(() => {
+        fetch(`http://localhost:8080/api/creditInfo/${customerNo}`)
+          .then((response) => response.json())
+          .then((data) => setCreditInfo(data))
+          .catch((error) => console.error(error));
+      }, []);
+
   return (
     <>
       <Toolbar/>
-            <div style={{ flex: 1, marginLeft: "30px" }}>
-                  <h1>환영합니다. {memberInfo.name}님의 정보입니다. </h1>
-                  <h3>여기 신용 정보 불러올 예정{memberInfo.financial_no} </h3>
-                  <h3>나의 등급 : {memberInfo.financial_no} </h3>
-                  <h3>나의 한도 : {memberInfo.financial_no} </h3>
-                  <h1>전체 정보와 비교해보세요</h1>
-            </div>
+            <Container>
+              <Card variant="outlined">
+                <CardContent>
+                  <Typography variant="h4">환영합니다. {memberInfo.name}님의 정보입니다.</Typography>
+                  {creditInfo && (
+                    <div>
+                      <Typography variant="h6">🔸고객 번호: {creditInfo.customerNo}</Typography>
+                      <Typography variant="h6">🔸대출 번호: {creditInfo.adNo}</Typography>
+                      <Typography variant="h6">🔸신용평가 점수: {creditInfo.res_Add}</Typography>
+                      <Typography variant="h6">🔸대출 한도: {creditInfo.pre_LMT}원</Typography>
+                    </div>
+                  )}
+                  <Typography variant="h5">나의 한도 수준을 전체 정보와 비교해보세요</Typography>
+                </CardContent>
+              </Card>
+            </Container>
 
-             <div>
-                                <div style={{ display: "flex" , marginLeft: "30px" }}>
-                                 <iframe src="http://192.168.30.156:5601/app/dashboards#/create?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:15,i:'54999c5e-fea1-4262-8610-9276a8df8bf9',w:24,x:0,y:0),id:bd08dc80-0c22-11ee-9f9b-a5cedd435e90,panelIndex:'54999c5e-fea1-4262-8610-9276a8df8bf9',type:lens,version:'7.11.1'),(embeddableConfig:(),gridData:(h:15,i:bf054f88-b19c-48ef-b151-62f6defbbeb1,w:24,x:24,y:0),id:f1dd3240-0c26-11ee-9f9b-a5cedd435e90,panelIndex:bf054f88-b19c-48ef-b151-62f6defbbeb1,type:lens,version:'7.11.1')),query:(language:kuery,query:''),tags:!(),timeRestore:!f,title:'',viewMode:edit)&hide-filter-bar=true" height="600" width="800"
-                                    height="600"
-                                    width="800"
-                                  ></iframe>
-                                  <div style={{ flex: 1, marginLeft: "30px" }}>
-                                    <h2 style={{ fontWeight: 'bold' }}>시각화 설명</h2>
-                                    <p style={{ fontWeight: 'bold' }}>1️⃣️  신용 평가 등급 별 한도 수준</p>
-                                    <p> ➜ 1등급의 가장 많은 한도 수준은 1,200만원이며,</p>
-                                    <p> ➜ 7등급의 가장 많은 한도 수준은 520만원으로 결과가 나왔습니다.</p>
-
-                                    <p style={{ fontWeight: 'bold' }}>2️⃣  신용 평가 등급 별 1년간 총 연체 건수</p>
-                                    <p> ➜ 가장 많은 연체 건수 : 10건</p>
-                                    <p> 5등급 = 18명</p>
-                                    <p> 8등급 = 12명</p>
-                                    <p> ➜ 가장 적은 연체 건수 : 0건</p>
-                                    <p> 2등급 = 2,880명</p>
-                                    <p> 4등급 = 1,796명</p>
-                                  </div>
-
-                                </div>
-              </div>
-    </>
+            <Container>
+            <Card style={{ marginTop: '30px' }}>
+            <iframe src="http://192.168.30.156:5601/app/dashboards#/create?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:15,i:'54999c5e-fea1-4262-8610-9276a8df8bf9',w:24,x:0,y:0),id:bd08dc80-0c22-11ee-9f9b-a5cedd435e90,panelIndex:'54999c5e-fea1-4262-8610-9276a8df8bf9',type:lens,version:'7.11.1'),(embeddableConfig:(),gridData:(h:15,i:bf054f88-b19c-48ef-b151-62f6defbbeb1,w:24,x:24,y:0),id:f1dd3240-0c26-11ee-9f9b-a5cedd435e90,panelIndex:bf054f88-b19c-48ef-b151-62f6defbbeb1,type:lens,version:'7.11.1')),query:(language:kuery,query:''),tags:!(),timeRestore:!f,title:'',viewMode:edit)&hide-filter-bar=true" height="600" width="800"
+                                                                    height="600"
+                                                                    width="800"
+                                                                  ></iframe>
+            </Card>
+              <Grid container spacing={2} style={{ marginTop: '30px' }}>
+                <Grid item xs={12} sm={6}>
+                  <Card variant="outlined">
+                    <CardContent>
+                      <Typography variant="h6" style={{ fontWeight: 'bold' }}>시각화 설명</Typography>
+                      <Typography variant="body1" style={{ fontWeight: 'bold' }}>1️⃣️  신용 평가 등급 별 한도 수준</Typography>
+                      <Typography variant="body2">➜ 1등급의 가장 많은 한도 수준은 1,200만원이며,</Typography>
+                      <Typography variant="body2">➜ 7등급의 가장 많은 한도 수준은 520만원으로 결과가 나왔습니다.</Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Card variant="outlined">
+                    <CardContent>
+                      <Typography variant="h6" style={{ fontWeight: 'bold' }}>시각화 설명</Typography>
+                      <Typography variant="body1" style={{ fontWeight: 'bold' }}>2️⃣  신용 평가 등급 별 1년간 총 연체 건수</Typography>
+                      <Typography variant="body2">➜ 가장 많은 연체 건수 : 10건</Typography>
+                      <Typography variant="body2">5등급 = 18명</Typography>
+                      <Typography variant="body2">8등급 = 12명</Typography>
+                      <Typography variant="body2">➜ 가장 적은 연체 건수 : 0건</Typography>
+                      <Typography variant="body2">2등급 = 2,880명</Typography>
+                      <Typography variant="body2">4등급 = 1,796명</Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Grid>
+            </Container>
+          </>
   );
 }
